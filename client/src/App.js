cb37b0cebb9e54ee49746644ab5ada7daadfc22d
David Lindahl <davidalindahl@gmail.com>
2018-04-30 14:43:37 -0700

    minor updates to the copy

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Made With Spark</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          a carefully curated selection of projects made with Spark.
         </p>
       </div>
     );
