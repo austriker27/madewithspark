a859f822c2c523bfe2df8083a74f388dafd0d55a
David Lindahl <davidalindahl@gmail.com>
2018-05-01 08:29:52 -0700

    updates to the site config and theme for mws

--- client/src/utils/siteConfig.js
+++ client/src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'GCN',
-  siteDescription: 'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
-  siteUrl: 'https://gcn.netlify.com' // Site domain. Do not inculde a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  siteTitle: 'Made With Spark',
+  siteDescription: 'A currated collection of projects made with Laravel Spark.',
+  siteUrl: 'https://madewithspark.com' // Site domain. Do not inculde a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
 };
